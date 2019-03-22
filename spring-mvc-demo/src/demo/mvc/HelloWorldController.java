e13c042b9bed486524a59d443cb37d47d8e6fd0f
shantiox <danaciocoiu78@gmail.com>
2019-03-22 13:50:56 +0200

    Reading HTML Form Data with @RequestParam Annotation

--- spring-mvc-demo/src/demo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/demo/mvc/HelloWorldController.java
@@ -5,42 +5,58 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-	
-	
-	//a controller method to show the initial HTML form
+	// a controller method to show the initial HTML form
 	@RequestMapping("/showForm")
 	public String showForm() {
-		return"helloworld-form";
+		return "helloworld-form";
 	}
-	
-	//a controller method to process the HTML form
+
+	// a controller method to process the HTML form
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
 	}
-	
-	//new a controller method to read form data and add data to the model
-	
+
+	// new a controller method to read form data and add data to the model
+
 	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
-		
-		//read the request parameter from the HTML form
+
+		// read the request parameter from the HTML form
 		String theName = request.getParameter("studentName");
-		
-		//convert data to all caps
+
+		// convert data to all caps
 		theName = theName.toUpperCase();
-		
-		//create the message
-		String result ="Yo! " + theName;
-		
-		
-		//add message to the model
+
+		// create the message
+		String result = "Yo! " + theName;
+
+		// add message to the model
 		model.addAttribute("message", result);
-		
+
 		return "helloworld";
 	}
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName,
+			Model model) {
+
+	
+		// convert data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloworld";
+	
+}
 }
